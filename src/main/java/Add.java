deb2cc1b7ff5518043e2556bb902089139fdd626
Surya Teja <Surya Teja@LAPTOP-N3Q913SL>
2021-01-17 18:55:13 +0530

    lambda expression

---
+++ src/main/java/Add.java
@@ -0,0 +1,19 @@
+package src.main.java;
+
+
+ 
+public class Add {
+
+	public static int addition(int i,int j) {
+		return i+j;
+	}
+	
+/*	public static void main(String[] args) {
+		
+		Add obj = new Add();
+		int i=10,j=20;
+		int res = obj.addition(i, j);
+		System.out.println(res);
+	}*/
+
+}
---
+++ src/main/java/TestAdd.java
@@ -0,0 +1,16 @@
+package src.main.java;
+
+
+import src.main.java.Add;
+import org.junit.Test;
+import static org.junit.Assert.*;
+ 
+
+public class TestAdd  {
+
+	  @Test  
+	    public void testFindMax(){  
+	        assertEquals(21,Add.addition(-10,11));  
+	        assertEquals(29,Add.addition(18,11));   
+	    }  
+}
